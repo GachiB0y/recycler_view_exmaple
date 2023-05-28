79ae56307c3d289d22dcf336fdef643f10453a7f
Volkov  A <maloletka2222@mail.ru>
2023-05-28 22:43:10 +0300

    add Bottom Nav Bar Items

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recyclerviewexample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.GridLayoutManager
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+        bottomNav()
+        binding.bNav.selectedItemId = R.id.edititemview
         launcherEditScreen = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             if (it.resultCode == RESULT_OK){
                 adapter.addItem(it.data?.getSerializableExtra("item") as Item)
@@ -32,6 +35,20 @@ class MainActivity : AppCompatActivity() {
             buttonAdd.setOnClickListener{
                 launcherEditScreen?.launch(Intent(this@MainActivity, EditItemActivity::class.java))
             }
+        }
+    }
+
+    private  fun bottomNav() {
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item2 ->{
+                    Toast.makeText(this, "Item", Toast.LENGTH_SHORT)
+                }
+                R.id.edititemview ->{
+                    launcherEditScreen?.launch(Intent(this@MainActivity, EditItemActivity::class.java))
+                }
+            }
+            true
         }
     }
 }
